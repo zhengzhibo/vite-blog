ba1029984c6d4c90c52ced1bcd2bcdc01ca7a914
Alex Zheng <zhibo90@gmail.com>
2020-07-15 22:33:08 +0800

    footer header to pagefooter pageheader

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>PageFooter</footer>
+</template>
+
+<script>
+export default {
+  name: 'PageFooter',
+};
+</script>
+
+<style scoped>
+footer {
+  background-color: black;
+}
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,64 @@
+<template>
+  <header>
+    <div class="header-wrapper">
+      <div class="header-logo-container">
+        <a href="/">
+          <img class="header-logo-img" src="../assets/logo.png" />
+        </a>
+      </div>
+      <div class="header-nav-container">
+        <nav class="header-nav-list">
+          <div class="header-nav-item">
+            <router-link to="/">Home</router-link>
+          </div>
+          <div class="header-nav-item">
+            <router-link to="/tags">Tags</router-link>
+          </div>
+          <div class="header-nav-item">
+            <router-link to="/about">About</router-link>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'PageHeader',
+};
+</script>
+
+<style scoped>
+.header-wrapper {
+  position: relative;
+  width: 100%;
+  padding-top: 2.8vw;
+  padding-bottom: 2.8vw;
+  padding-left: 6vw;
+  padding-right: 6vw;
+  pointer-events: auto;
+}
+
+.header-logo-container {
+  width: auto;
+}
+
+.header-logo-img {
+  height: 60px;
+}
+
+.header-nav-container {
+  width: 33%;
+}
+
+.header-nav-list {
+    display: inline-flex;
+    flex-wrap: wrap;
+}
+
+.header-nav-item {
+    margin-left: .75vw;
+    margin-right: .75vw;
+}
+</style>
