15d96790edd01f9384bc8421a57fdbe7f7a50bb8
Alex Zheng <zhibo90@gmail.com>
2020-07-15 22:30:57 +0800

    remove sass

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from './routes';
 
-import './index.scss';
+import './index.css';
 
 createApp(App).use(router).mount('#app');
